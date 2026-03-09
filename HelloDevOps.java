5d31edf1a738bc571f9b6f73e0364b5f2e2aabd2
bhumikurmi <bhumikurmi97@gmail.com>
2026-03-10 00:33:17 +0530

    Added Practical 5 files

---
+++ HelloDevOps.java
@@ -0,0 +1,6 @@
+public class HelloDevOps {
+    public static void main(String[] args) {
+        System.out.println("Cloud Computing with DevOps Practical");
+        System.out.println("This project demonstrates GitHub documentation.");
+    }
+}
